bbbf55461f6cf2fe7d7ed728a8a5bd9a1a9c589d
MrFriday <71829924+MrFriday1@users.noreply.github.com>
2023-03-31 20:37:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const sandwichNames = ["Hot Sandwich", "Pickle Sanwich", "Spicy Sandwich", "Tasty Sandwich", "Terrible Sandwich"]
 
+function sandwichGenerator() {
+    let sandwich = sandwichNames[Math.floor(Math.random() * sandwichNames.length)];
+    document.querySelector("sandwichText").innerHTML = sandwich;
+}
